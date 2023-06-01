2cefd373751946f2ee7cd93eaea763b5d0466b58
bhismapratama <bhismaelki@gmail.com>
2023-06-01 11:12:27 +0700

    nambah perubahan homepage bang

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+// BAGIAN NAVBAR
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
+
+// TYPE BAGIAN HOMEPAGE
+var typed = new Typed('#element', {
+    strings: ['Excelsior', "Angkatan '22"],
+    typeSpeed: 120,
+    loop: true
+});
