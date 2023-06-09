33cd35686e0c6094701b1e14ce1d6f54fbeabae2
bhismapratama <bhismaelki@gmail.com>
2023-06-09 14:33:27 +0700

    responsive logos

--- public/js/main.js
+++ public/js/main.js
@@ -9,17 +9,17 @@ const stars1 = document.getElementById('stars1');
 // let stars2 = document.getElementById('stars2');
 // let stars3 = document.getElementById('stars3');
 
-window.addEventListener('scroll', function(){
-    const value = this.window.scrollY;
-    // console.log({value : value})
-    stars.style.top = value * 0.35 + 'px';
-    stars1.style.left = value * 0.1 + '%';
-    // stars2.style.left = value * 0.1 + 'px';
-})
+// window.addEventListener('scroll', function(){
+//     const value = this.window.scrollY;
+//     // console.log({value : value})
+//     stars.style.top = value * 0.35 + 'px';
+//     stars1.style.left = value * 0.1 + '%';
+//     // stars2.style.left = value * 0.1 + 'px';
+// })
 
-// TYPE BAGIAN HOMEPAGE
-let typed = new Typed('#element', {
-    strings: ['Excelsior', "Angkatan '22"],
-    typeSpeed: 120,
-    loop: true
-});
+// // TYPE BAGIAN HOMEPAGE
+// let typed = new Typed('#element', {
+//     strings: ['Excelsior', "Angkatan '22"],
+//     typeSpeed: 120,
+//     loop: true
+// });
