a16afe868c831af5059829479a877eaea1928cdc
bhismapratama <bhismaelki@gmail.com>
2023-06-15 20:28:12 +0700

    bro

---
+++ public/js/member.js
@@ -0,0 +1,14 @@
+const card = document.querySelectorAll('.card')
+const popup = document.querySelectorAll('.popup')
+
+for(let i = 0; i < card.length; i++) {
+    card[i].addEventListener('click', () => {
+        popup[i].classList.toggle('open')
+    })
+}
+
+for(let i = 0; i < popup.length; i++) {
+    popup[i].addEventListener('click', () => [
+        popup[i].classList.toggle('open'),
+    ])
+}
